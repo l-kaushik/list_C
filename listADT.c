3326c2f54c7aca1a2d07f8e1aaa4cc3ae086b9ca
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-06-26 10:27:55 +0530

    createList function for initialization of list

--- listADT.c
+++ listADT.c
@@ -1,14 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct list
+typedef struct myArray
 {
     int total_size;
-};
+    int used_size;
+    int *base_ptr;
+} list;
 
+void createList(list *addressOfObject, int tSize, int uSize)
+{
+    addressOfObject->total_size = tSize;
+    addressOfObject->used_size = uSize;
+    addressOfObject->base_ptr = (int *) malloc(tSize*sizeof(int));
 
+}
 
 int main()
 {
-    
+    list marks;
+
+    createList(&marks,20,10);
+
     return 0;
 }
