ddd56f5b332069c3b5ce761963ff99e226dea704
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-07-03 02:29:19 +0530

    Create insert() function This function takes 3 arguments: pointer to a list, index, and an element. It will insert the element in the given index. Index must be within the range of AOF->used_size;

--- listADT.c
+++ listADT.c
@@ -12,21 +12,22 @@ typedef int bool;
 #define false 0
 #endif
 
-
 #define AOF address_of_object
-#define NULL_CHECK(ptr) do { \
-    if ((ptr) == NULL) { \
-        printf("Null pointer detected at %s:%d\n", __FILE__, __LINE__); \
-        exit(EXIT_FAILURE); \
-    } \
-} while (0)
-
+#define NULL_CHECK(ptr)                                                     \
+    do                                                                      \
+    {                                                                       \
+        if ((ptr) == NULL)                                                  \
+        {                                                                   \
+            printf("Null pointer detected at %s:%d\n", __FILE__, __LINE__); \
+            exit(EXIT_FAILURE);                                             \
+        }                                                                   \
+    } while (0)
 
 typedef struct myArray
 {
     int total_size;
     int used_size;
-    int *base_ptr; 
+    int *base_ptr;
     bool isAllocated;
 } list;
 
@@ -41,7 +42,8 @@ void createList(list *AOF, int tSize)
 
 void show(list *AOF)
 {
-    if (AOF->isAllocated != true){
+    if (AOF->isAllocated != true)
+    {
         printf("Error: Memory is not allocated for list");
         exit(EXIT_FAILURE);
     }
@@ -51,8 +53,9 @@ void show(list *AOF)
     for (int i = 0; i < AOF->used_size; i++)
     {
         printf("%d", AOF->base_ptr[i]);
-        if (i < AOF->used_size - 1) {
-        printf(", ");
+        if (i < AOF->used_size - 1)
+        {
+            printf(", ");
         }
     }
     printf("]");
@@ -65,10 +68,11 @@ void setValues(list *AOF, int count, ...)
     int availSize = AOF->total_size - AOF->used_size;
     int newSize = AOF->total_size + count - availSize;
 
-    if (count > availSize)
+    if (count >= availSize)
     {
-        AOF->base_ptr = (int *)realloc(AOF->base_ptr, newSize*sizeof(int));
-        NULL_CHECK(AOF->base_ptr);   
+        AOF->base_ptr = (int *)realloc(AOF->base_ptr, newSize * sizeof(int));
+        AOF->total_size = newSize;
+        NULL_CHECK(AOF->base_ptr);
     }
 
     int startIndex = AOF->used_size;
@@ -82,11 +86,38 @@ void setValues(list *AOF, int count, ...)
     va_end(args);
 }
 
+void insert(list *AOF, int index, int element)
+{
+    if(index > AOF->used_size)
+    {
+        printf("Error: index out of range, %d is the last index",AOF->used_size);
+        exit(EXIT_FAILURE);
+    }
+
+    if(AOF->used_size <= AOF->total_size)
+    {
+        AOF->base_ptr = (int *)realloc(AOF->base_ptr, (AOF->total_size + 1) * sizeof(int));
+        AOF->total_size += 1;
+        NULL_CHECK(AOF->base_ptr);
+    }
+
+    for (int i = AOF->used_size - 1; i >= index; i--)
+    {
+        AOF->base_ptr[i+1] = AOF->base_ptr[i];
+    }
+
+    AOF->base_ptr[index] = element;
+    AOF->used_size += 1;
+
+}
+
 int main()
 {
     list marks;
     createList(&marks, 10);
-    setValues(&marks, 5, 1,2,3,4,5);
+    setValues(&marks, 5, 1, 2, 3, 4, 5);
+    show(&marks);
+    insert(&marks, 5, 25);
     show(&marks);
     free(marks.base_ptr);
 
