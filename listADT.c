7df366e173330cc0b1dcb155e5af7f84dcc92695
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-07-03 01:19:15 +0530

    Major fixes and setValues() function 1. define a way for boolean values, this is beneficial in case where try to access the list without allocating memory to it. 2. created isAllocated variable in the structure for the checking purposes. 3. setValues() is now the standalone function for storing/appending variable no. of values in it.
    NOTE: while using the setValues(), no. of variadic values should be equals to the count or the 2nd argument of the setValues() function, otherwise it will result in lose of values or garbage values in the list.

--- listADT.c
+++ listADT.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <assert.h>
+
+// defining bool for older versions
+#if __STDC_VERSION__ >= 19990L
+#include <stdbool.h>
+#else
+typedef int bool;
+#define true 1
+#define false 0
+#endif
+
 
 #define AOF address_of_object
 #define NULL_CHECK(ptr) do { \
     if ((ptr) == NULL) { \
         printf("Null pointer detected at %s:%d\n", __FILE__, __LINE__); \
+        exit(EXIT_FAILURE); \
     } \
 } while (0)
 
@@ -15,25 +27,25 @@ typedef struct myArray
     int total_size;
     int used_size;
     int *base_ptr; 
+    bool isAllocated;
 } list;
 
 void createList(list *AOF, int tSize)
 {
     AOF->total_size = tSize;
     AOF->used_size = 0;
-    AOF->base_ptr = (int *)malloc(tSize * sizeof(int));
+    AOF->base_ptr = (int *)calloc(tSize, sizeof(int));
     NULL_CHECK(AOF->base_ptr);
+    AOF->isAllocated = true;
 }
 
 void show(list *AOF)
 {
-
-    assert(AOF->used_size > 0);
-
-    if(AOF->used_size > 0){
-        printf("Error: No values are present in the list\n");
-        return;
+    if (AOF->isAllocated != true){
+        printf("Error: Memory is not allocated for list");
+        exit(EXIT_FAILURE);
     }
+    assert(AOF->used_size > 0);
 
     for (int i = 0; i < AOF->used_size; i++)
     {
@@ -41,26 +53,16 @@ void show(list *AOF)
     }
 }
 
-void setValue(list *AOF, int count)
-{
-    for (int i = 0; i < count; i++)
-    {
-        printf("Enter element %d\n", i);
-        scanf("%d", &(AOF->base_ptr[i]));
-    }
-
-    AOF->used_size += count;
-}
-
 void setValues(list *AOF, int count, ...)
 {
     va_list args;
     va_start(args, count);
     int availSize = AOF->total_size - AOF->used_size;
+    int newSize = AOF->total_size + count - availSize;
 
     if (count > availSize)
     {
-        AOF->base_ptr = (int *)realloc(AOF->base_ptr, (AOF->total_size + count - availSize)*sizeof(int));
+        AOF->base_ptr = (int *)realloc(AOF->base_ptr, newSize*sizeof(int));
         NULL_CHECK(AOF->base_ptr);   
     }
 
@@ -69,9 +71,7 @@ void setValues(list *AOF, int count, ...)
     for (int i = 0; i < count; ++i)
     {
         AOF->base_ptr[startIndex + i] = va_arg(args, int);
-
     }
-    printf("value in used_size is %d\t",AOF->used_size);
     AOF->used_size += count;
 
     va_end(args);
@@ -81,14 +81,8 @@ int main()
 {
     list marks;
     createList(&marks, 10);
-
-    setValue(&marks, 3);
-    setValues(&marks, 5, 1,2,3,4,5);
-    setValues(&marks, 5, 6,7,8,9,10);
-    setValues(&marks, 5, 11,12,13,14,15);
-
+    setValues(&marks, 7, 1,2,3,4,5);
     show(&marks);
-
     free(marks.base_ptr);
 
     return 0;
